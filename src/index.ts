6e6fb96e2eb26d73dfed703e949a5d083a53fcd3
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2018-05-13 00:22:01 +0300

    -

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,11 @@ import { GraphQLServer } from 'graphql-yoga'
 import { createConnection } from "typeorm";
 import { genSchema } from "./utils/genSchema";
 
-const server = new GraphQLServer({
-  schema: genSchema(),
-});
-
 createConnection().then(() => {
-  server.start({
-    port:   process.env.PORT || 4000
-  }).then(() => console.log('Server is running on localhost:4000'));
+  const server = new GraphQLServer({
+    schema: genSchema(),
+  });
+
+  server.start();
+  console.log('Server is running on localhost ' + process.env.POST || 4000);
 });
